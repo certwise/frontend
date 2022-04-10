d70aa5df877e74a06394f04b8177c307ed565dcd
Sivaram Shabari <sivaramsbr1@gmail.com>
2022-04-10 17:13:02 +0530

    :wrench: Comment out datepicker

--- src/components/ui/Datepicker.tsx
+++ src/components/ui/Datepicker.tsx
@@ -23,20 +23,22 @@ function Datepicker({ align }: any) {
 	};
 
 	return (
-		<div className="relative">
-			<Flatpickr
-				className="form-input pl-9 text-gray-500 hover:text-gray-600 font-medium focus:border-gray-300 w-60"
-				options={options as any}
-			/>
-			<div className="absolute inset-0 right-auto flex items-center pointer-events-none">
-				<svg
-					className="w-4 h-4 fill-current text-gray-500 ml-3"
-					viewBox="0 0 16 16"
-				>
-					<path d="M15 2h-2V0h-2v2H9V0H7v2H5V0H3v2H1a1 1 0 00-1 1v12a1 1 0 001 1h14a1 1 0 001-1V3a1 1 0 00-1-1zm-1 12H2V6h12v8z" />
-				</svg>
+		<>
+			<div className="relative">
+				{/* <Flatpickr
+					className="form-input pl-9 text-gray-500 hover:text-gray-600 font-medium focus:border-gray-300 w-60"
+					options={options as any}
+				/> */}
+				<div className="absolute inset-0 right-auto flex items-center pointer-events-none">
+					<svg
+						className="w-4 h-4 fill-current text-gray-500 ml-3"
+						viewBox="0 0 16 16"
+					>
+						<path d="M15 2h-2V0h-2v2H9V0H7v2H5V0H3v2H1a1 1 0 00-1 1v12a1 1 0 001 1h14a1 1 0 001-1V3a1 1 0 00-1-1zm-1 12H2V6h12v8z" />
+					</svg>
+				</div>
 			</div>
-		</div>
+		</>
 	);
 }
 
